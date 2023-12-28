8a4d749b90ed7312aac66f8c64026165e8b5304e
lkbrian <lkbrian742@gmail.com>
2023-12-28 19:26:52 +0300

    updates

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# Phase-1-SA-03-Code-Challenge-Flatacuties
+# Phase 1 SA 03 Code Challenge Flatacuties
+
+introduction 
+
+In this code challenge it was about using fetch to retrieve 
+data from the server and using it to manipulate the DOM 
+ ihave 4 files for the challenge 
+
+ ## index.html
+
+ this is the file where the names and the animas get displayed this is
+ the file that javascripts m,anipulates an displays the contents
+
+ ## style.css
+
+ thi the file that used to style m index html and the html elements that were created 
+ in the javascript file that performs what is required for the test
+
+ ## db.json
+
+ this is the file nthat stores the dat from where the json server  retrieves
+ data from 
+
+ ## script.js
+
+ this is the file that performs the required test or rather functions.
+ Steps
+ 1. first fetch the data from the server then use tha data acquired to display on the DOM
+2. i displayed the names by creating an element wihin a function and append it to the existing ul element in the htm file we use forEach method to get and display al the animal names
+
+3. i created another function to fetch animals using their id's
+4. from the data we acquired in th prevoius step  we build a card used to display the specific animals
+5. from which we added the event listener to the  the list elemet we created earlier to show the specific animal when clicked
+
+6. we added the functionality to the specific buttons
+that is to vote and clear
--- script.js
+++ script.js
@@ -1,33 +1,66 @@
+//getting the data from our db.json via the server
 function fetching() {
   return fetch("http://localhost:3000/characters")
     .then((res) => res.json())
     .then((data) => buildCharacter(data));
 }
+//calling the function to actually get the data back
 fetching();
 
-function buildCharacter(character) {
+//function to build the names that will get displayed in our DOM
+function buildCharacter(characters) {
   const ul = document.querySelector("ul");
-  character.forEach((element) => {
+  characters.forEach((element) => {
     const li = document.createElement("li");
     li.innerHTML = element.name;
+    li.addEventListener('click', () => fetchAnimal(element.id)); 
     ul.appendChild(li);
   });
 }
 
+//fetch the specific animals from the server using the animals id
 
+function fetchAnimal(animalId) { 
+  fetch(`http://localhost:3000/characters/${animalId}`)
+    .then(response => response.json())
+    .then(animal => {
+      buildAnimal(animal);
+    })
+}
+//function to build the animal that gets displayed with animal is clicked details
 function buildAnimal(animal) {
+  const existingAnimalCard = document.querySelector(".animal-Card");
+  if (existingAnimalCard) {
+    existingAnimalCard.remove();
+  }
   let animalcard = document.createElement("div");
   animalcard.className = "animal-Card";
   animalcard.innerHTML = `
-  <img src="${animal.image}" alt="${animal.name}">
-  <p>Name: ${animal.name}</p>
-  <p>Votes: <span id="votes">${animal.votes}</span></p>
-  <div class="buttons">
-    <button id="vote" onclick="addVote(${animal.id})">Vote</button> 
-    <p id="vote-count">Votes: <span id="votes">${animal.votes}</span></p>
-    <button id="clear">Clear</button>
-  </div>`;
-  animalcard.style.display = "block";
-  document.querySelector("ul").appendChild(animalcard);
+    <img src="${animal.image}" alt="${animal.name}">
+    <p>Name: ${animal.name}</p>
+    <p id="votes">Votes: ${animal.votes}</p>
+    <div class="buttons">
+      <button id="vote" >Vote</button> 
+      <button id="clear">Clear</button>
+    </div>`;
+
+    let voteCount = animal.votes; 
+
+    animalcard.querySelector('#vote').addEventListener('click', function () {
+
+      voteCount++;
+      animalcard.querySelector('#votes').innerText = `Votes: ${voteCount}`;
+    });
+
+    let clear = animal.votes; 
+
+    animalcard.querySelector('#clear').addEventListener('click', function () {
+     voteCount = 0;
+    animalcard.querySelector('#votes').innerText = `Votes: ${clear}`;
+    });
+  let showAnimal=document.querySelector('ul')
+  showAnimal.appendChild(animalcard)
 }
-buildAnimal()
+
+
+
